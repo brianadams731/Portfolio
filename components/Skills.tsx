5557be26c659c76034690f5a4f7eabacbb7c9dcb
brianadams731 <brianadams731@gmail.com>
2022-05-26 12:18:35 -0700

    Changed: Flicker animation to only flicker skill grid

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -8,20 +8,20 @@ const Skills = () => {
     useEffect(()=>{
         const flicker = setInterval(()=>{
             const rand = Math.random();
-            if(rand > .93){                
+            if(rand > .95){                
                 wrapperRef.current?.animate([
                     {transform: "skewX(0)"},
-                    {transform: "skewX(-50deg)"},
+                    {transform: "skewX(-30deg)"},
                     {transform: "skewX(0)"},                    
-                ],{duration:20})
+                ],{duration:28})
             }
         },300)
         return ()=> clearInterval(flicker);
     },[])
     return (
-        <article className={styles.wrapper} ref={wrapperRef}>
+        <article className={styles.wrapper}>
             <h2 className={styles.hero}>Frequently Used Tech</h2>
-            <div className={styles.gridWrapper}>
+            <div className={styles.gridWrapper} ref={wrapperRef}>
                 <SkillGrid />
                 <div className={styles.distortedGrid}>
                     <SkillGrid />
