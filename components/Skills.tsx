b1b700b4482cc4a24fe5b732cab0fb12ccfe02a0
brianadams731 <brianadams731@gmail.com>
2022-05-26 20:22:52 -0700

    Changed: Flicker to each individual skill element

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -5,21 +5,22 @@ import { SkillGrid } from "./SkillGrid";
 const Skills = () => {
     const wrapperRef = useRef<HTMLDivElement>(null);
 
-    useEffect(()=>{
+    /*useEffect(()=>{
         const flicker = setInterval(()=>{
             const rand = Math.random();
-            if(rand > .94){                
+            if(rand > .90){                
                 wrapperRef.current?.animate([
                     {transform: "skewX(0)"},
-                    {transform: rand>.96?"skewX(-25deg)":"skew(25deg)"},
+                    {transform: rand>.95?"skewX(-15deg)":"skew(15deg)"},
                     {transform: "skewX(0)"},                    
-                ],{duration:28})
+                ],{duration:25})
             }
         },300)
         return ()=> clearInterval(flicker);
-    },[])
+    },[])*/
+    
     return (
-        <article className={styles.wrapper}>
+        <article id="skills" className={styles.wrapper}>
             <h2 className={styles.hero}>Frequently Used Tech</h2>
             <div className={styles.gridWrapper} ref={wrapperRef}>
                 <SkillGrid />
--- components/TechUsed.tsx
+++ components/TechUsed.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { noLoad } from "../utils/imgLoaders";
 import styles from "../styles/TechUsed.module.scss";
+import { useEffect, useRef } from "react";
 
 interface Props{
     imgURL:string;
@@ -8,8 +9,24 @@ interface Props{
 }
 
 const TechUsed = ({imgURL, name}:Props):JSX.Element => {
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    
+    useEffect(()=>{
+        const flicker = setInterval(()=>{
+            const rand = Math.random();
+            if(rand > .98){                
+                wrapperRef.current?.animate([
+                    {transform: "skewX(0)"},
+                    {transform: rand>.98?"skewX(-28deg)":"skew(28deg)"},
+                    {transform: "skewX(0)"},                    
+                ],{duration:28})
+            }
+        },300)
+        return ()=> clearInterval(flicker);
+    },[])
+
     return(
-        <div className={styles.wrapper}>
+        <div className={styles.wrapper} ref={wrapperRef}>
             <div className={styles.imgWrapper}>
                 <Image layout="fill" src={imgURL} loader={noLoad} className={styles.imgOne} unoptimized />
             </div>
